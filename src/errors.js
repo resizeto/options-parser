19edc44e484bceeeb71e12dd5cf1fc16cbaff02e
Brandon Aaron <hello.brandon@aaron.sh>
2019-11-20 18:04:05 -0500

    Add names for error objects

--- src/errors.js
+++ src/errors.js
@@ -1,7 +1,23 @@
-class OptionValueInvalidError extends Error {}
-class OptionKeyUnknownError extends Error {}
-class SignatureMismatchError extends Error {}
-class SignatureMissingError extends Error {}
+class OptionValueInvalidError extends Error {
+  get name () {
+    return 'OptionValueInvalidError'
+  }
+}
+class OptionKeyUnknownError extends Error {
+  get name () {
+    return 'OptionKeyUnknownError'
+  }
+}
+class SignatureMismatchError extends Error {
+  get name () {
+    return 'SignatureMismatchError'
+  }
+}
+class SignatureMissingError extends Error {
+  get name () {
+    return 'SignatureMissingError'
+  }
+}
 
 module.exports = {
   OptionValueInvalidError,
